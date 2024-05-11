be6853e3cc74b058e615f855b7447ecee9b70ceb
tremarian <p.shaimova@aspro.ru>
2024-05-11 22:10:13 +0500

    Homework for first lesson

---
+++ lesson1/lesson_1_task_7.py
@@ -0,0 +1,14 @@
+def func_params(num):
+    print(num, end='')
+
+func_params(8)
+func_params(8)
+func_params(0)
+func_params(0)
+func_params(5)
+func_params(5)
+func_params(5)
+func_params(3)
+func_params(5)
+func_params(3)
+func_params(5)
