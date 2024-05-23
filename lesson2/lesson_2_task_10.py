3ddcda4837f91b487c1a484d46fc3bf7919b387a
tremarian <p.shaimova@aspro.ru>
2024-05-23 16:52:12 +0500

    task 10

--- lesson2/lesson_2_task_10.py
+++ lesson2/lesson_2_task_10.py
@@ -1,3 +1,7 @@
 def bank(x,y):
-    for x in (1,y):
-        x = x 
+    for n in range (0,y):
+        x = x + (x / 100 * 10)
+    print(f"Сумма накоплений: {x}")
+x = int(input("Введите сумму вклада: "))
+y = int(input("Введите срок вклада в годах: "))
+bank(x,y)
