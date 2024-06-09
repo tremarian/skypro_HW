628e0716699d0c12153801c49e50a6841614d466
tremarian <p.shaimova@aspro.ru>
2024-06-09 23:30:01 +0500

    Calculator added

---
+++ lesson4/calculator.py
@@ -0,0 +1,28 @@
+class Calculator:
+    def sum(self, a, b):
+        return a+b
+    
+    def sub(self, a, b):
+       return a-b
+    
+    def mul(self, a, b):
+        return a*b
+    
+    def div(self, a, b):
+        if (b == 0):
+            raise ArithmeticError("На ноль делить нельзя")
+        return a/b 
+    
+    def pow(self, a, b=2):
+        return a**b
+    
+    def avg(self, nums):
+        if (len(nums) == 0):
+            return 0
+
+        s = 0
+        for num in nums:
+             s = s + num
+
+        l = len(nums)
+        return self.div(s, l)
